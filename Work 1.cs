0ebe12dd4509a76b814d6112bb01bb6b484475a5
Dima Malyavka <dima_malyavka@mail.ru>
2024-02-24 02:22:13 +0300

    Решение задачи и создание блок схемы

--- Work 1.cs
+++ Work 1.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] str = new string[4] {"Hello", "2", "world", ":-"};
+string [] array = new string[4];
+void FindNewArray(string[] str, string[] array, int j = 0)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+           array[j] = str[i];
+           j++;
+        }
+    }
+}
+void PrintArray(string[] mass)
+{
+    for(int k = 0; k < array.Length; k++)
+    {
+        Console.Write($"{array[k]}" + "\t");
+    }
+    Console.WriteLine();
+}
+FindNewArray(str, array);
+ PrintArray(array);
+
